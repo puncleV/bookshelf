ea3e519d99e5659a82ab143b3b864dd88ac680a8
Victor <punkrus18@gmail.com>
2019-07-10 09:43:43 +0300

    Update book-repository.ts
    fix name for book repo fields map 2

--- src/repositories/book-repository.ts
+++ src/repositories/book-repository.ts
@@ -1,12 +1,12 @@
 import {BaseRepository, IBaseRepositoryDependencies} from "./base-repository";
-import {PizzaFieldsMap} from "./constants";
+import {BookFieldsMap} from "./constants";
 import {IBook, IRawBook} from "./types";
 
 export class BookRepository extends BaseRepository<IBook, IRawBook> {
   constructor(dependencies: IBaseRepositoryDependencies) {
     super(dependencies, {
       entity: "book",
-      mapToRawFields: PizzaFieldsMap,
+      mapToRawFields: BookFieldsMap,
     });
   }
 }
